0e787eed0e6ba021a9589f64baed5f8c0712e674
Pranav Iyer <piyer8@asu.edu>
2025-02-22 22:31:45 -0700

    Added signal plots

---
+++ src/types/SignalTypes.tsx
@@ -0,0 +1,19 @@
+export interface Signal {
+    signal_index: string;
+    sensory_descriptions: string[];
+    emotion_descriptions: string[];
+    associative_descriptions: string[];
+    sensory_keywords: string[];
+    emotional_keywords: string[];
+    associative_keywords: string[];
+}
+
+export interface EmotionalKeyword {
+    word: string;
+    emotion: string;
+    intensity: number;
+}
+export interface SignalEmotions {
+    signal_index: string;
+    keywords: EmotionalKeyword[];
+}
